05ebb6553c835b6c079ba036c5de2894c9eccdf9
Dorin-hub <doreenjerotich545@gmail.com>
2022-10-19 13:26:35 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that does the following
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+
+	{putchar(n);
+		n += 1;
+	}
+	printf("\n");
+	return (0);
+}
