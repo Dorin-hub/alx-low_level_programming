c052dedd4d29357a59c03b184411286eb791bd8e
Dorin-hub <doreenjerotich545@gmail.com>
2022-10-19 12:43:46 -0700

    Task  4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+char l;
+l = 'a';
+	while
+	(l <= 'z') {
+			if ((l != 'e' && l != 'q') && l <= 'z')
+				putchar(l);
+			l++;
+		}
+	putchar('\n');
+	return (0);
+}
